ac25d8dbd7b7e58a98609c350e0160115a98b822
LSC <953548030@qq.com>
2023-03-29 17:45:58 +0800

    初始化项目

---
+++ src/page/login.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-form ref="form" :model="form" :rules="loginRules" label-width="80px">
+    <el-form-item label="用户名" >
+      <el-input v-model="form.userName" placeholder=请输入用户名></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input type="password" v-model="form.password" placeholder=请输入密码></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-checkbox v-model="form.rememberMe">记住我</el-checkbox>
+      <el-button type="text" @click="onForgotPassword">忘记密码？</el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">登录</el-button>
+      <el-button type="primary" @click="onRegister">注册</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'login',
+  data() {
+    return {
+      form: {
+        userName: 'test',
+        password: '123456',
+        rememberMe: false
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      axios.post('/api/user/login', this.form)
+        .then(response => {
+          const token = response.data;
+          Cookies.set('token', token.data);
+          this.router.push('/');
+        }).catch(error =>{
+          //todo 
+          console.log("失败");
+      
+        });
+    },
+    onRegister() {
+      // 模拟跳转到注册页面
+      this.router.push('/register');
+    }
+  },
+  //设置跳转路由
+  setup() {
+    const router = useRouter();
+    return { router };
+  }
+};
+</script>
+
+
+<style>
+/* 居中,悬浮效果 */
+.el-form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+body {
+  background-image: url('../assets/login2.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- xs（最小号 ）	<768px
+        sm（小号）	≥768px
+        md（中号）	≥992px
+        lg（大号）	≥1200px
+        xl（更大号）	≥1920px -->
+  <div class="my-page" style="width: 100%; height: 100%;">
+    <!-- 这里的样式会覆盖element-ui的默认样式 -->
+    <el-container style="width: 100%; height: 100%;">
+      <!-- 头部 -->
+      <el-header style="height: 5%;">Header</el-header>
+      <!-- 躯干 -->
+      <el-main style="height: 90%;">
+        <!-- 水平布局 -->
+        <el-row type="flex" style="width: 100%;height: 100%;">
+          <!-- 左侧导航 -->
+          <el-col :span="4" >
+            <el-menu default-active="1" class="el-menu-vertical-demo">
+              <el-menu-item index="1" icon="el-icon-house">首页</el-menu-item>
+              <el-menu-item index="2" icon="el-icon-s-shop">商城</el-menu-item>
+              <el-menu-item index="3" icon="el-icon-s-data">数据</el-menu-item>
+              <el-menu-item index="4" icon="el-icon-setting">设置</el-menu-item>
+            </el-menu>
+          </el-col>
+          <!-- 中间主栏 -->
+          <el-col :span="20">Main</el-col>
+        </el-row>
+      </el-main>
+      <!-- 尾部 -->
+      <el-footer style="height: 5%;">Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<style>
+.my-page .el-col {
+  border: 1px solid black;
+}
+</style>
+
+<script>
+  export default {
+    name: 'Home'
+  };
+</script>
+
